d9722fec391ae7e425f0ae911a3176b55ca84717
michaelramoiloa <ramoiloamichael@gmail.com>
2023-05-04 04:16:40 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,40 +1,25 @@
-#include "lists.h"
-
-/**
- * free_listint_safe - frees a linked list
- * @h: pointer to the first node in the linked list
- *
- * Return: number of elements in the freed list
- */
-size_t free_listint_safe(listint_t **h)
-{
-	size_t len = 0;
-	int diff;
-	listint_t *temp;
-
-	if (!h || !*h)
-		return (0);
-
-	while (*h)
-	{
-		diff = *h - (*h)->next;
-		if (diff > 0)
-		{
-			temp = (*h)->next;
-			free(*h);
-			*h = temp;
-			len++;
-		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-			len++;
-			break;
-		}
-	}
-
-	*h = NULL;
-
-	return (len);
-}
+1 #include <stdlib.h>                                                             
+  2 #include "lists.h"                                                              
+  3                                                                                 
+  4 /**                                                                             
+  5  * add_nodeint adds a new node at the beginning of a linked list                
+  6  * @head: pointer to the first node in the list                                 
+  7  * @n: data to insert in that new node                                          
+  8  * Return: pointer to the new node, or NULL if it fails                         
+  9  */                                                                             
+10                                                                                 
+11 listint_t *add_nodeint(listint_t **head, const int n)                           
+12 {                                                                               
+13         listint_t *new;                                                         
+14                                                                                 
+15         new = malloc(sizeof(listint_t));                                        
+16         if (new == NULL)                                                        
+17                 return (NULL);                                                  
+18                                                                                 
+19         new->n = n;                                                             
+20         new->next = *head;                                                      
+21         *head = new;                                                            
+22                                                                                 
+23         return (*head);                                                         
+24 }                                                                               
+~             
